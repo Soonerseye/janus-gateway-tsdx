7d43686f4608b7c55cce6e0fd89a9029f2b5cf58
Soonerseye <mccoylstevens0@gmail.com>
2024-10-27 05:43:21 -0400

    h

--- test/utilts/index.ts
+++
@@ -1,6 +0,0 @@
-type Constructor<T = object> = new (...args: any[]) => T;
-
-// author: https://github.com/stevenulibarri/jest-typescript-mock-reference
-export const instantiateClassMock = <T>(Clazz: Constructor<T>): jest.Mocked<T> => {
-  return new Clazz() as jest.Mocked<T>;
-};
